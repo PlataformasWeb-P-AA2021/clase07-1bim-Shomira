138f0297fcd92027abd67e005945fbcbd2371e7c
Shomira <shomirarosales01@gmail.com>
2021-05-20 14:15:56 -0500

    lectura de archivos txt, agregar datos a la base y consultas

--- ejemplo02/ingreso_datos.py
+++ ejemplo02/ingreso_datos.py
@@ -15,29 +15,35 @@ engine = create_engine(cadena_base_datos)
 
 Session = sessionmaker(bind=engine)
 session = Session()
+
+#Lectura del archivos
 datos_jugadores = open ('data/datos_jugadores.txt','r', encoding="utf8")
 datos_clubs = open ('data/datos_clubs.txt','r', encoding="utf8")
-#for f in f:
 
-#session.query(Club).filter_by(nombre="LDU").one()
 contenido_j=datos_jugadores.readlines()
 contenido_c=datos_clubs.readlines()
-#print(contenido
 
 #Datos Clubs
 c=''
-for i in range (0,len(contenido_c),1): # split por ;
-   d=contenido_c[i].split(";")
+for i in range (0,len(contenido_c),1): 
+   d=contenido_c[i].split(";") # split por ;
    p = Club(nombre=d[0], deporte=d[1], fundacion=d[2])
    session.add(p)
 
+#Consulta para obtener todos los datos de los clubs 
+clubs = session.query(Club).all() 
 d=''
 #Datos de Jugadores
 for i in range (0,len(contenido_j),1): # split por ;
    d=contenido_j[i].split(";")
+   #Ciclo para evaluar el nombre del club a los que pertenecen los jiugadores
+   #para asignarles el id del club al que pertenecen
+   for club in clubs:
+      if d[0] == club.nombre:
+         id_club = club.id
 
-   p = Jugador(nombre=d[3], dorsal=d[2], posicion=d[1])
+   p = Jugador(nombre=d[3], dorsal=d[2], posicion=d[1],club_id=id_club)
    session.add(p)
-
+# confirmacion de transacciones
 session.commit()
 
